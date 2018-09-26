4f9c4927b2a65101a30d2bbb6f818ba44e0f333f
Tomasz Pach <tomaszpach@icloud.com>
2018-09-26 09:44:29 +0200

    remove unused parts

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Inputs from './components/inputs/index';
 import RickAndMortyCharactersCards from './containers/RickAndMortyCharactersCards';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = {  };
     }
-
-    onSubmit(e) {
-
-        console.log('get input value');
-        console.log(this.state.value);
-
-
-        e.preventDefault();
-
-    }
-
-    handleChange = (e) => {
-        this.setState({ value: e.target.value });
-    };
 
     render() {
         return (
             <div className="App">
-                {/*<form onSubmit={(e) => this.onSubmit(e)}>*/}
-                    {/*<label>*/}
-                        {/*Name:*/}
-                        {/*<input type="text" name="name" value={this.state.value} onChange={(e) => this.handleChange(e)} />*/}
-                    {/*</label>*/}
-                    {/*<input type="submit" value="Submit" />*/}
-                {/*</form>*/}
-                {/*<Inputs />*/}
                 <RickAndMortyCharactersCards />
             </div>
         );
