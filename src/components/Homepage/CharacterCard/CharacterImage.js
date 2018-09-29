abf31e241c1f921707ce199380ac1c2444929f9e
tomaszpach <tomaszpach@icloud.com>
2018-09-29 16:01:30 +0200

    add correct alt for images

--- src/components/Homepage/CharacterCard/CharacterImage.js
+++ src/components/Homepage/CharacterCard/CharacterImage.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const CharacterImage = ({image}) => <img src={image} alt="test"/>;
+const CharacterImage = ({image, name}) => <img src={image} alt={name}/>;
 
 export default CharacterImage;
