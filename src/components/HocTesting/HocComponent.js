cd215eca0515fce2d60debfc3f8200dc995e1037
tomaszpach <tomaszpach@icloud.com>
2018-09-29 12:45:28 +0200

    comment logs from HOC testing components

--- src/components/HocTesting/HocComponent.js
+++ src/components/HocTesting/HocComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default (Component) => {
+    // console.log('component', Component);
     return (props) => (
         <div style={{backgroundColor: props.backgroundColor || 'red'}}>
             <Component {...props} />
--- src/components/HocTesting/HocEnhancedApp.js
+++ src/components/HocTesting/HocEnhancedApp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import EnhancedHoc from './HocComponent';
 
 const HocEnhancedApp = (props) => {
+    // console.log('hocEnhancedApp props', props);
     return (
         <p>Hello enhanced App (Higher-order component) {props.foo}</p>
     );
